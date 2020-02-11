6e5a1d20a789419f0a6ed15fe5d8cca3eda9c0e1
Marcos Batista <markos.system@gmail.com>
2020-02-10 23:15:40 -0300

    Correções no arquivo .env

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 module.exports = {
-  url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-rmg1e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  url: `${process.env.DB_HOST}`
 };
